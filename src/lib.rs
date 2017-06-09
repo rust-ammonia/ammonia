8c3cd92e8459163896d4a73f7c3953f011ec4845
Bastien Orivel <eijebong@bananium.fr>
2017-06-09 20:24:36 +0200

    Update html5ever to 0.17 and bump version to 0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,18 @@
 //! [html5ever]: https://github.com/servo/html5ever "The HTML parser in Servo"
 //! [jsocol's Bleach]: https://github.com/jsocol/bleach
 
-extern crate html5ever;
 #[macro_use]
-extern crate html5ever_atoms;
+extern crate html5ever;
 #[macro_use]
 extern crate maplit;
 #[macro_use]
 extern crate tendril;
 extern crate url;
-use html5ever::driver as html;
-use html5ever::rcdom::{RcDom, NodeEnum, Handle};
+use html5ever::{driver as html, QualName};
+use html5ever::rcdom::{RcDom, NodeData, Handle};
 use html5ever::serialize::{serialize, SerializeOpts, TraversalScope};
-use html5ever::tree_builder::interface::{NodeOrText, TreeSink};
+use html5ever::tree_builder::{NodeOrText, TreeSink};
 use std::collections::{HashMap, HashSet};
-use std::mem::swap;
 use tendril::stream::TendrilSink;
 use url::Url;
 
@@ -118,40 +116,35 @@ impl<'a> Ammonia<'a> {
     /// algorithm also takes care of things like unclosed and (some) misnested
     /// tags.
     pub fn clean(&self, src: &'a str) -> String {
-        let mut parser = html::parse_fragment(RcDom::default(), html::ParseOpts::default(), qualname!("", "div"), vec![]);
+        let mut parser = html::parse_fragment(RcDom::default(), html::ParseOpts::default(), QualName::new(None, ns!(), local_name!("div")), vec![]);
         parser.process(format_tendril!("{}", src));
         let mut dom = parser.finish();
         let mut stack = Vec::new();
         let body = {
-            let document = dom.document.borrow_mut();
-            document.children[0].clone()
+            let children = dom.document.children.borrow();
+            children[0].clone()
         };
         stack.push(body.clone());
-        while stack.len() != 0 {
-            let node_handle = stack.pop().unwrap();
+        while !stack.is_empty() {
+            let node = stack.pop().unwrap();
             let mut has_children = {
-                let node = node_handle.borrow_mut();
-                match &node.node {
-                    &NodeEnum::Comment(_) | &NodeEnum::Text(_) | &NodeEnum::Doctype(_, _, _) => false,
-                    &NodeEnum::Document | &NodeEnum::Element(_, _, _) => true,
+                match node.data {
+                    NodeData::Comment{..} | NodeData::Text{..} | NodeData::Doctype{..} | NodeData::ProcessingInstruction{..} => false,
+                    NodeData::Document | NodeData::Element{..} => true,
                 }
             };
             while has_children {
-                let mut children = Vec::new();
-                {
-                    let mut node = node_handle.borrow_mut();
-                    swap(&mut node.children, &mut children);
-                }
+                let mut children = std::mem::replace(&mut *node.children.borrow_mut(), Vec::new());
+
                 for child in &mut children {
-                    self.clean_child(&mut dom, child, node_handle.clone());
+                    self.clean_child(&mut dom, child, node.clone());
                 }
                 {
-                    let node = node_handle.borrow_mut();
-                    has_children = node.children.len() != children.len();
+                    has_children = node.children.borrow().len() != children.len();
                 }
             }
-            let node = node_handle.borrow_mut();
-            stack.extend(node.children.clone());
+
+            stack.extend(node.children.borrow().iter().cloned());
         }
         let mut ret_val = Vec::new();
         let opts = SerializeOpts{
@@ -161,16 +154,15 @@ impl<'a> Ammonia<'a> {
         serialize(&mut ret_val, &body, opts).unwrap();
         String::from_utf8(ret_val).unwrap()
     }
+
     fn clean_child(&self, dom: &mut RcDom, child: &mut Handle, parent: Handle) {
         let pass = {
-            let mut child = child.borrow_mut();
-            let mut child = &mut *child;
-            match &mut child.node {
-                &mut NodeEnum::Text(_) => true,
-                &mut NodeEnum::Comment(_) => !self.strip_comments,
-                &mut NodeEnum::Doctype(_, _, _) |
-                &mut NodeEnum::Document => false,
-                &mut NodeEnum::Element(ref name, _, ref mut attrs) => {
+            match child.data {
+                NodeData::Text{..} => true,
+                NodeData::Comment{..} => !self.strip_comments,
+                NodeData::Doctype{..} |
+                NodeData::Document | NodeData::ProcessingInstruction{..} => false,
+                NodeData::Element{ref name, ref attrs, ..} => {
                     let safe_tag = {
                         if self.tags.contains(&*name.local) {
                             let attr_filter = |attr: &html5ever::Attribute| {
@@ -192,22 +184,20 @@ impl<'a> Ammonia<'a> {
                                 }
                             };
                             if self.keep_cleaned_elements {
-                                attrs.retain(attr_filter);
+                                attrs.borrow_mut().retain(attr_filter);
                                 true
                             } else {
-                                attrs.iter().all(attr_filter)
+                                attrs.borrow().iter().all(attr_filter)
                             }
                         } else {
                             false
                         }
                     };
+
                     if !safe_tag {
-                        for sub in &mut child.children {
-                            {
-                                let mut sub = sub.borrow_mut();
-                                sub.parent = None;
-                            }
-                            dom.append(parent.clone(), NodeOrText::AppendNode(sub.clone()));
+                        for sub in child.children.borrow_mut().iter_mut() {
+                            sub.parent.replace(None);
+                            dom.append(&parent.clone(), NodeOrText::AppendNode(sub.clone()));
                         }
                     }
                     safe_tag
@@ -215,11 +205,8 @@ impl<'a> Ammonia<'a> {
             }
         };
         if pass {
-            {
-                let mut child = child.borrow_mut();
-                child.parent = None;
-            }
-            dom.append(parent, NodeOrText::AppendNode(child.clone()));
+            child.parent.replace(None);
+            dom.append(&parent.clone(), NodeOrText::AppendNode(child.clone()));
         }
     }
 }
