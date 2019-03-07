c4f139e7f14d0493821ea6929984a4d4623d32b6
Michael Howell <michael@notriddle.com>
2019-03-06 18:24:39 -0700

    Add `clean_text` function
    Fixes #76

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,7 @@ use std::str::FromStr;
 use tendril::stream::TendrilSink;
 use tendril::StrTendril;
 pub use url::Url;
+use std::cmp::max;
 
 lazy_static! {
     static ref AMMONIA: Builder<'static> = Builder::default();
@@ -80,6 +81,55 @@ lazy_static! {
 ///     assert_eq!(ammonia::clean("XSS<script>attack</script>"), "XSS")
 pub fn clean(src: &str) -> String {
     AMMONIA.clean(src).to_string()
+}
+
+/// Turn an arbitrary string into unformatted HTML.
+///
+/// This function is roughly equivalent to PHP's `htmlspecialchars` and `htmlentities`.
+/// it is maximally strict on purpose, encoding every character that has special meaning to the
+/// HTML parser.
+///
+/// This function cannot be used to package strings into a `<script>` or `<style>` tag;
+/// you need a JavaScript or CSS escaper to do that.
+///
+/// It also does not make user text safe for HTML attribute microsyntaxes such as `class` or `id`.
+/// Only use this function for places where HTML accepts unrestricted text such as `title` attributes
+/// and paragraph contents.
+pub fn clean_text(src: &str) -> String {
+    let mut ret_val = String::with_capacity(max(4, src.len()));
+    for c in src.chars() {
+        ret_val.push_str(match c {
+            // this character, when confronted, will start a tag
+            '<' => "&lt;",
+            // in an unquoted attribute, will end the attribute value
+            '>' => "&gt;",
+            // in an attribute surrounded by double quotes, this character will end the attribute value
+            '\"' => "&quot;",
+            // in an attribute surrounded by single quotes, this character will end the attribute value
+            '\'' => "&apos;",
+            // in HTML5, returns a bogus parse error in an unquoted attribute, while in SGML/HTML, it will end an attribute value surrounded by backquotes
+            '`' => "&grave;",
+            // in an unquoted attribute, this character will end the attribute
+            '/' => "&#47;",
+            // starts an entity reference
+            '&' => "&amp;",
+            // if at the beginning of an unquoted attribute, will get ignored
+            '=' => "&#61;",
+            // will end an unquoted attribute
+            ' ' => "&#32;",
+            '\t' => "&#9;",
+            '\n' => "&#10;",
+            '\r' => "&#12;",
+            // a spec-compliant browser will perform this replacement anyway, but the middleware might not
+            '\0' => "&#65533;",
+            // ALL OTHER CHARACTERS ARE PASSED THROUGH VERBATIM
+            _ => {
+                ret_val.push(c);
+                continue;
+            }
+        });
+    }
+    ret_val
 }
 
 /// An HTML sanitizer.
@@ -2471,5 +2521,10 @@ mod test {
         Builder::new()
             .clean_content_tags(hashset!["a"])
             .clean("");
+    }
+
+    #[test]
+    fn clean_text_test() {
+        assert_eq!(clean_text("<this> is <a test function"), "&gt;this&lt;&#32;is&#32;&gt;a&#32;test&#32;function");
     }
 }
